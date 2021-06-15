c0488f3ddc8567ae3a092a0baede6ce97c554a7f
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-15 02:03:46 +0200

    Add files via upload

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,73 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.wildswap.finance/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.wildswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/
+      },
+
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/wildswap/',
+      },
+
+    ],
+  },
+
+]
+
+export default config
